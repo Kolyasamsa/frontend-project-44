83912510f8e5f0bd795f3ff8fc555690e932a953
Kolyasamsa <kristinakolesnikova819@gmail.com>
2024-01-04 23:33:22 +0500

    update src/getRandomNum.js

--- src/getRandomNum.js
+++ src/getRandomNum.js
@@ -1,4 +1,7 @@
-const getRandomNumber = () => Math.round(Math.random() * 100);
-// eslint-disable-next-line eol-last
+const getRandomNumber = (min = 0, max = 100) => {
+  // случайное число от min до (max+1)
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
 
 export default getRandomNumber;
