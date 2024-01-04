3aac33b56643391e2583dd7973ee1184a85de4dc
Kolyasamsa <kristinakolesnikova819@gmail.com>
2024-01-04 21:42:28 +0500

    add src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,23 @@
+import getRandomNumber from '../getRandomNum.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (a, b) => {
+  let num1 = a;
+  let num2 = b;
+  while (num2 !== 0) {
+    const temp = num1 % num2;
+    num1 = num2;
+    num2 = temp;
+  }
+  return num1;
+};
+
+const startRound = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const question = `Question: ${num1} ${num2}`;
+  const correctAnswer = gcd(num1, num2);
+  return [question, correctAnswer];
+};
+export { description, startRound };
